5dbdc4f574711b11c5cac3d91a31d604497eabdd
rodrigocuadra <30705964+rodrigocuadra@users.noreply.github.com>
2019-06-08 14:04:51 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -27,6 +27,8 @@ echo -e "\n"
 echo -e "************************************************************"
 echo -e "*                 Generate credentials                     *"
 echo -e "************************************************************"
+python -m pip install --upgrade google-auth-oauthlib[tool]
+google-oauthlib-tool --scope https://www.googleapis.com/auth/assistant-sdk-prototype --scope https://www.googleapis.com/auth/gcm --save --headless --client-secrets /var/lib/asterisk/$client_secret
 echo -e "\n"
 echo -e "************************************************************"
 echo -e "*                        Copy Files                        *"
